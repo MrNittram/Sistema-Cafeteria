f091c81c4e527d66f29f31f8a781752ded8f88af
MrNittram <daniel.inostroza2005@gmail.com>
2026-09-16 00:51:28 -0300

    Creación de Navbar y sección Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+function Hero() {
+    return (
+      <section className="hero">
+        <div className="hero__content">
+          <p className="hero__subtitle">Café recién preparado</p>
+  
+          <h1>Tu café favorito, a un clic</h1>
+  
+          <p className="hero__description">
+            Explora nuestros productos y crea tu pedido de manera rápida y sencilla.
+          </p>
+  
+          <button className="hero__button">
+            Ver productos
+          </button>
+        </div>
+      </section>
+    )
+  }
+  
+  export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <nav>
+            <h2>CaféSahur</h2>
+
+            <div>
+            <a href="#">Inicio</a>
+            <a href="#">Productos</a>
+            <a href="#">Categorias</a>
+            <button>Carrito</button>
+
+            </div>
+        </nav>
+    )
+}
+export default Navbar
