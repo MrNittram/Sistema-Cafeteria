8d1313c3c604827d3acaa96e3ab7e2f1f9081abe
MrNittram <daniel.inostroza2005@gmail.com>
2026-09-23 21:28:53 -0300

    componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+function Footer () {
+    return (
+        <footer className="Footer">
+            <h3>
+                CaféSahur☕️
+            </h3>
+            <p>
+                Encuentra tu café favorito y arma tu pedido.
+            </p>
+            <p>
+                Equipo de desarrollo y gestión:
+            </p>
+            <div>
+                className="footer_developers"
+                <a href="https://github.com/MrNittram">
+                    @MrNittram
+                </a>
+                <a href="https://github.com/Cris-0523">
+                    @Cris-0523
+                </a>
+            </div>
+            <small>
+            © 2026 CaféSahur. Todos los derechos reservados.
+            </small>
+        </footer>
+    )
+}
+export default Footer
