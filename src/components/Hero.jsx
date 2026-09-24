e88ea5d677cc2d4b5501059a3718903415f8492e
MrNittram <daniel.inostroza2005@gmail.com>
2026-09-24 14:25:50 -0300

    implementacion hero button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,12 @@ function Hero() {
     className="hero"
     >
       <h1>CaféSahur</h1>
-      <p>Encuentra tu café favorito y arma tu pedido.</p>
+      <p>
+        Encuentra tu café favorito y arma tu pedido.
+        </p>
+        <a href="#productos" className="hero__button">
+          Ver Menú
+        </a>
     </section>
   )
 }
