3a1804225676d6a5dfb26683e39be326cef9e811
MrNittram <daniel.inostroza2005@gmail.com>
2026-09-25 11:49:36 -0300

    mejora diseño buscador de productos

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -2,11 +2,12 @@ function Buscador({ busqueda, cambiarBusqueda }) {
     return (
       <div className="buscador">
         <input
-          type="text"
-          placeholder="Buscar productos..."
-          value={busqueda}
-          onChange={(event) => cambiarBusqueda(event.target.value)}
-        />
+  className="buscador__input"
+  type="text"
+  placeholder="Buscar productos..."
+  value={busqueda}
+  onChange={(event) => cambiarBusqueda(event.target.value)}
+/>
       </div>
     )
   }
