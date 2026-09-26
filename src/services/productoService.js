b97384a17ab6cbd69e4913732ca297b8dd4375f8
Cris-0523 <cristobalalmendra67@gmail.com>
2026-09-25 23:23:20 -0300

    Gran Reset

--- src/services/productoService.js
+++ src/services/productoService.js
@@ -1,9 +1,4 @@
-import { getProductos as getProductosAPI } from "../api/productos";
-
-
-export function getProductos(){
-
-    return getProductosAPI()
-        .then((respuesta) => respuesta.data);
-
+export function getProductos() {
+    return fetch('/data/productos.json')
+        .then((respuesta) => respuesta.json())
 }
